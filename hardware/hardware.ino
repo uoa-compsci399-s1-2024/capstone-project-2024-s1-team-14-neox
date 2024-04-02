2edc779b4de58e1b5b5df4b028dd64734adc8459
Kevin Lu <90817513+theman156352@users.noreply.github.com>
2024-04-02 22:50:25 +1300

    Hardware - Add sensor polling code (#16)
    * Hardware - Add sensor polling code
    
    * Hardware - Move eepromBegin error handling into the function itself
    
    ---------
    
    Co-authored-by: Kevin Lu <>

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -1,9 +1,45 @@
+#include <Wire.h>
+#include "sensor_sample.h"
+#include "eeprom.h"
+#include "imu.h"
+#include "rtc.h"
+
+static const unsigned int SERIAL_BAUD_RATE = 9600;
+static const uint32_t POLL_INTERVAL_MS = (uint32_t)60 * 1000; // 1 minute
+static const uint8_t UV_SENSOR_PIN = A0;
+static const uint8_t LIGHT_SENSOR_PIN = A1;
+
+// Read all sensors and save them to the EEPROM
+static void readSample();
+
 void setup()
 {
-  
+  Serial.begin(SERIAL_BAUD_RATE);
+  Wire.begin();
+
+  initializeIMU();
+  initializeRTC(0, 0);
+  eepromBegin();
 }
 
 void loop()
 {
+  static uint32_t lastSampleReadTime = 0;
+  uint32_t now = millis();
 
+  if (now - lastSampleReadTime >= POLL_INTERVAL_MS)
+  {
+    lastSampleReadTime += POLL_INTERVAL_MS;
+    readSample();
+  }
+}
+
+static void readSample()
+{
+  SensorSample sample = { 0 };
+  sample.timestamp = readRTC();
+  sample.uv = analogRead(UV_SENSOR_PIN);
+  sample.light = analogRead(LIGHT_SENSOR_PIN);
+  sample.acceleration = readIMU();
+  eepromPushSample(&sample);
 }
--- hardware/sensor_sample.h
+++ hardware/sensor_sample.h
@@ -1,8 +1,19 @@
 #ifndef _SENSOR_SAMPLE_H
 #define _SENSOR_SAMPLE_H
 
+#include "rtc.h"
+
+/*
+ * The uv and light fields store the output of analogRead().
+ * To convert this value to a voltage, see
+ * https://www.arduino.cc/reference/en/language/functions/analog-io/analogread/
+ */
+
 struct SensorSample {
-  // data...
+  time timestamp;
+  uint16_t uv;
+  uint16_t light;
+  uint8_t acceleration;
 } __attribute__((packed));
 
 #endif
