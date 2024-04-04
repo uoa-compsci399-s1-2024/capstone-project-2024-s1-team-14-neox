e4e5cadeffda6e90ee87aebe58ca9b2d02d4c3a8
luciaskim <97372163+luciaskim@users.noreply.github.com>
2024-04-05 11:46:23 +1300

    App: Added child profile tile for ui

--- app/lib/child_home/presentation/tiles/child_profile_tile.dart
+++ app/lib/child_home/presentation/tiles/child_profile_tile.dart
@@ -1,4 +1,6 @@
+import 'package:age_calculator/age_calculator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../data/child_model.dart';
 
@@ -11,8 +13,41 @@ class ChildProfileTile extends StatefulWidget {
 }
 
 class _ChildProfileTileState extends State<ChildProfileTile> {
+
+  String calculateAge(DateTime dateOfBirth) {
+    DateDuration duration = AgeCalculator.age(dateOfBirth);
+    return "${duration.years} years, ${duration.months} months";
+  }
+
+
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          const Icon(
+            Icons.face,
+            size: 100,
+          ),
+          const Text("Name"),
+          Text(
+            widget.profile.name,
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          const Text("Age"),
+          Text(
+            calculateAge(widget.profile.dateOfBirth),
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          
+        ],
+      ),
+    );
   }
 }
