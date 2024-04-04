c1511e5258268a4d16830b7e90e37c60d7d8bf74
luciaskim <97372163+luciaskim@users.noreply.github.com>
2024-04-04 23:17:46 +1300

    App: Implement bloc for bluetooth state management

--- app/lib/bluetooth/bloc/bluetooth_bloc.dart
+++ app/lib/bluetooth/bloc/bluetooth_bloc.dart
@@ -1,13 +1,49 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'dart:async';
+import 'dart:io';
+import '../../data/child_repository.dart';
 
 part 'bluetooth_event.dart';
 part 'bluetooth_state.dart';
 
 class BluetoothBloc extends Bloc<BluetoothEvent, BluetoothState> {
-  BluetoothBloc() : super(BluetoothInitial()) {
-    on<BluetoothEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  // Refer to data repository
+  final ChildRepository _childRepository;
+
+  List<BluetoothDevice> _systemDevices = [];
+  List<ScanResult> _scanResults = [];
+  bool _isScanning = false;
+  late StreamSubscription<List<ScanResult>> _scanResultsSubscription;
+  late StreamSubscription<bool> _isScanningSubscription;
+
+
+
+  BluetoothBloc(this._childRepository) : super(BluetoothState()) {
+    on<BluetoothScanStartPressed>(_onBluetoothScanStartPressed);
+    on<BluetoothScanStopPressed>(_onBluetoothScanStopPressed);
+    on<BluetoothPairPressed>(_onBluetoothPairPressed);
+    on<BluetoothUnpairPressed>(_onBluetoothUnpairPressed);
+    on<BluetoothConnectPressed>(_onBluetoothConnectPressed);
+    on<BluetoothSyncPressed>(_onBluetoothSyncPressed);
+  }
+
+  FutureOr<void> _onBluetoothScanStartPressed(BluetoothScanStartPressed event, Emitter<BluetoothState> emit) {
+  }
+
+  FutureOr<void> _onBluetoothScanStopPressed(BluetoothScanStopPressed event, Emitter<BluetoothState> emit) {
+  }
+
+  FutureOr<void> _onBluetoothPairPressed(BluetoothPairPressed event, Emitter<BluetoothState> emit) {
+  }
+
+  FutureOr<void> _onBluetoothUnpairPressed(BluetoothUnpairPressed event, Emitter<BluetoothState> emit) {
+  }
+
+  FutureOr<void> _onBluetoothConnectPressed(BluetoothConnectPressed event, Emitter<BluetoothState> emit) {
+  }
+
+  FutureOr<void> _onBluetoothSyncPressed(BluetoothSyncPressed event, Emitter<BluetoothState> emit) {
   }
 }
