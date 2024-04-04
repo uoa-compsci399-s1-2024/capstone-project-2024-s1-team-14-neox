8566cb6088d7c8103d90f1cd3c9482e4c10945b4
hsir571 <79824690+hsir571@users.noreply.github.com>
2024-04-04 16:14:56 +1300

    data model

--- app/lib/Entities/arduinoData.dart
+++ app/lib/Entities/arduinoData.dart
@@ -1,14 +1,71 @@
-
-import 'dart:collection';
-
+import 'package:capstone_project_2024_s1_team_14_neox/dB/database.dart';
 import 'package:drift/drift.dart';
 
-//@UseRowClass(ArduinoDataEntity)
+@UseRowClass(ArduinoDataEntity)
 class ArduinoDatas extends Table {
   TextColumn get name => text()();
   IntColumn get uv => integer()();
   IntColumn get light => integer()();
-  DateTimeColumn get dataTime => dateTime()();
+  DateTimeColumn get datetime => dateTime()();
 }
 
 
+class ArduinoDataEntity{
+  String? name;
+  int? uv;
+  int? light;
+  DateTime datetime;
+
+
+  ArduinoDataEntity(
+      this.name, this.uv, this.light, this.datetime
+      );
+
+  ArduinoDatasCompanion toCompanion() {
+    return ArduinoDatasCompanion(
+      name: Value(name ?? ''),
+      uv: Value(uv ?? -1),
+      light: Value(light ?? -1),
+      datetime: Value(datetime),
+    );
+  }
+
+
+  static Future<void> saveSingleArduinoDataEntity(ArduinoDataEntity arduinoDataEntity) async {
+    AppDb db = AppDb();
+    await db
+        .into(db.arduinoDatas)
+        .insertOnConflictUpdate(arduinoDataEntity.toCompanion());
+  }
+
+  static Future<void> saveListOfArduinoDataEntity(
+      List<ArduinoDataEntity> arduinoDataEntityList) async {
+    await Future.forEach(arduinoDataEntityList, (arduinoDataEntity) async {
+      await saveSingleArduinoDataEntity(arduinoDataEntity);
+    });
+  }
+
+  static Future<List<ArduinoDataEntity>> queryAllArduinoData() async {
+    AppDb db = AppDb();
+    List<ArduinoDataEntity> arduinoDataEntityList = await db.select(db.arduinoDatas).get();
+    return arduinoDataEntityList;
+  }
+
+  static Future<ArduinoDataEntity?> queryArduinoDataByName(String name) async {
+    AppDb db = AppDb();
+    ArduinoDataEntity? arduinoDataEntity = await (db.select(db.arduinoDatas)
+      ..where((tbl) => tbl.name.equals(name)))
+        .getSingleOrNull();
+    return arduinoDataEntity;
+  }
+
+// Example method for querying Arduino data by UV level
+  static Future<List<ArduinoDataEntity>> queryListOfArduinoDataByUVLevel(int uvLevel) async {
+    AppDb db = AppDb();
+    List<ArduinoDataEntity> arduinoDataEntityList = await (db.select(db.arduinoDatas)
+      ..where((tbl) => tbl.uv.equals(uvLevel)))
+        .get();
+    return arduinoDataEntityList;
+  }
+
+}
