38de343da3dc9fa3aeb567a34ff485671d53afe3
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-05 23:10:55 +1300

    Implement lambda for `GET /samples?<querystring>`

--- server/lambda/functions/action-samples-search/index.mjs
+++ server/lambda/functions/action-samples-search/index.mjs
@@ -1,8 +1,21 @@
+import {
+  setupDB,
+} from "/opt/nodejs/lib.mjs";
+
+let db = await setupDB();
+
+console.log("connecting");
+await db.connect();
+console.log("connection success")
+
 export const handler = async (event) => {
-  // TODO implement
+  let res = await db.query("SELECT * FROM samples");
   const response = {
     statusCode: 200,
-    body: JSON.stringify(event),
+    body: JSON.stringify(res.rows),
+    headers: {
+      "Content-Type": "application/json",
+    }
   };
   return response;
 };
