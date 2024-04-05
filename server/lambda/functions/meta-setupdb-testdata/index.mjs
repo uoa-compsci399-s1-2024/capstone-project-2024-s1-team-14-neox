6f8ef3c8c65c8adcddbcfc7b7bfe84714c0e7eb0
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-05 15:46:30 +1300

    Securely get DB credentials and connect and read/write

--- server/lambda/functions/meta-setupdb-testdata/index.mjs
+++ server/lambda/functions/meta-setupdb-testdata/index.mjs
@@ -6,11 +6,11 @@ import {
   SecretsManagerClient,
   GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
-
-const secret_name = "rds!db-b30fda56-acbf-4805-8307-cdaeef52cc95";
+import fs from "node:fs";
+import process from "node:process";
 
 const client = new SecretsManagerClient({
-  region: "ap-southeast-2",
+  region: process.env.AWS_REGION
 });
 
 let response;
@@ -18,7 +18,7 @@ let response;
 try {
   response = await client.send(
     new GetSecretValueCommand({
-      SecretId: secret_name,
+      SecretId: process.env.SECRET_ARN,
       VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
     })
   );
@@ -28,24 +28,32 @@ try {
   throw error;
 }
 
-const secret = response.SecretString;
+const secret = JSON.parse(response.SecretString);
 
 // Your code goes here
 import pg from 'pg';
 const { Client } = pg;
- 
+
 const db = new Client({
-  host: 'project-db.ci7bv0oxri6k.ap-southeast-2.rds.amazonaws.com',
-  port: 5432,
-  database: 'projectdb',
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: secret.username,
   user: secret.username,
   password: secret.password,
+  ssl: {
+    ca: [fs.readFileSync(process.env.SSL_CERT_FILE)]
+  },
 })
-await client.connect();
+console.log("connecting");
+await db.connect();
+console.log("connection success")
 const CREATE_TABLES_TEXT = `
 -- NOTE: THERE SHOULD BE EXTERNAL AND INTERNAL IDs
 
-DROP TABLE parents;
+DROP TABLE IF EXISTS samples;
+DROP TABLE IF EXISTS children;
+DROP TABLE IF EXISTS parents;
+
 CREATE TABLE parents (
        -- id INTEGER NOT NULL PRIMARY KEY,
        ext_id VARCHAR(50) NOT NULL PRIMARY KEY,
@@ -53,7 +61,6 @@ CREATE TABLE parents (
        lname VARCHAR(100) NOT NULL
 );
 
-DROP TABLE children;
 CREATE TABLE children (
        -- id INTEGER NOT NULL PRIMARY KEY,
        ext_id VARCHAR(50) NOT NULL PRIMARY KEY,
@@ -65,12 +72,13 @@ CREATE TABLE children (
        FOREIGN KEY (parent_ext_id) REFERENCES parents (ext_id)
 );
 
-DROP TABLE samples;
 CREATE TABLE samples (
        -- id INTEGER NOT NULL PRIMARY KEY,
-       ts TIMESTAMP WITH TIMEZONE NOT NULL PRIMARY KEY,
+       -- ts TIMESTAMP WITH TIMEZONE NOT NULL PRIMARY KEY,
+       -- Use timestamptz alias for TIMESTAMP WITH TIMEZONE because there were syntax errors when I sent the query to the DB in RDS
+       ts TIMESTAMPTZ NOT NULL PRIMARY KEY,
        -- child_id INTEGER NOT NULL,
-       child_ext_id INTEGER NOT NULL,
+       child_ext_id VARCHAR(50) NOT NULL,
        uv_index INTEGER,
        lux INTEGER,
        -- FOREIGN KEY (child_id) REFERENCES children (id)
@@ -78,15 +86,15 @@ CREATE TABLE samples (
 );
 
 INSERT INTO parents (ext_id, fname, lname) VALUES ('1', 'John', 'Cena');
-INSERT INTO children (ext_id, parent_ext_id, fname) VALUES ('1', '1', 'Bobby');
-INSERT INTO samples (child_ext_id, ts, uv_index, lux) VALUES ('1', '2024-02-01+12', 2, 1500);
+INSERT INTO children (ext_id, parent_ext_id, fname) VALUES ('22', '1', 'Bobby');
+INSERT INTO samples (child_ext_id, ts, uv_index, lux) VALUES ('22', '2024-02-01+12', 2, 1500);
 `;
 export const handler = async (event) => {
   await db.query(CREATE_TABLES_TEXT);
   const res_parents = await db.query("SELECT * FROM parents");
   const res_children = await db.query("SELECT * FROM children");
   const res_samples = await db.query("SELECT * FROM samples");
-  console.log(res_parents);
-  console.log(res_children);
-  console.log(res_samples);
+  console.dir(res_parents.rows);
+  console.dir(res_children.rows);
+  console.dir(res_samples.rows);
 };
