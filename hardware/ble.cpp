48bb2d76626cebe2b89bfe371fc3eaec3298a061
d-aun <140460323+d-aun@users.noreply.github.com>
2024-04-06 06:46:25 +1300

    Added code for device BLE

---
+++ hardware/ble.cpp
@@ -0,0 +1,186 @@
+#include <stdint.h>
+#include <ArduinoBLE.h>
+#include <algorithm>
+#include "eeprom.h"
+#include "rtc.h"
+#include "ble.h"
+#include "sensor_sample.h"
+
+BLEService sensorSamplesService("ba5c0000-243e-4f78-ac25-69688a1669b4");
+
+/*
+ * Data will be sent 5 BLE characteristics at a time. These characteristics will be updated dynamically (depending on how much data must be sent).
+ */
+BLECharacteristic samples_1("42b25f8f-0000-43de-92b8-47891c706106", BLERead, 512);
+BLECharacteristic samples_2("5c5ef115-0001-431d-8c23-52ff6ad1e467", BLERead, 512);
+BLECharacteristic samples_3("1fc0372f-0002-43f3-8cfc-1a5611b88062", BLERead, 512);
+BLECharacteristic samples_4("ff3d9730-0003-4aac-84e2-0861c1d000a6", BLERead, 512);
+BLECharacteristic samples_5("6eea8c3b-0004-4ec0-a842-6ed292e598dd", BLERead, 512);
+
+/*
+ *   BLE Characteristic for receiving acknowledgement from app that data has been processed and sample characteristics can be updated
+ */
+BLEStringCharacteristic update("f06c06bb-0005-4f4c-b6b4-a146eff5ab15", BLEWrite, 8);
+
+/*
+ * Byte arrays for initial value set to each characteristic and buffers that will be used to update characteristic values. Eeprom will be read
+ * and values will be placed in a numbered buffer. Once the numbered buffer is full, its value will be written to the corresponding numbered
+ * characteristic. Buffers are only added to if there are enough timestamps. If there are not enough, they remain empty.
+ */
+byte initial[512];
+byte buffer_1[512];
+byte buffer_2[512];
+byte buffer_3[512];
+byte buffer_4[512];
+byte buffer_5[512];
+
+const uint32_t dataPerCharacateristic = 511;
+const uint32_t maxData = dataPerCharacateristic * 5;
+
+void initializeBLE() {
+    if (!BLE.begin()) 
+    {
+        Serial.println("BLE failed to initiate");
+        delay(500);
+        while(1);
+    }
+
+    BLE.setLocalName("Neox Sens 1.0");
+
+    sensorSamplesService.addCharacteristic(samples_1);
+    sensorSamplesService.addCharacteristic(samples_2);
+    sensorSamplesService.addCharacteristic(samples_3);
+    sensorSamplesService.addCharacteristic(samples_4);
+    sensorSamplesService.addCharacteristic(samples_5);
+    sensorSamplesService.addCharacteristic(update);
+
+    BLE.addService(sensorSamplesService);
+    BLE.advertise();
+    
+}
+
+void checkConnection() {
+    BLEDevice central = BLE.central();
+    while (central.connected())
+    {
+        updateValues();
+        
+    }
+}
+
+void addSample(byte arr[512], uint32_t& index, SensorSample sample) {
+    arr[index] = sample.timestamp.hour;
+    index++;
+    arr[index] = sample.timestamp.minute;
+    index++;
+    arr[index] = (sample.uv & 0x00ff);
+    index++;
+    arr[index] = (sample.uv & 0xff00) >> 8;
+    index++;
+    arr[index] = (sample.light & 0x00ff);
+    index++;
+    arr[index] = (sample.light & 0xff00) >> 8;
+    index++;
+    arr[index] = sample.acceleration;
+    index++;
+}
+
+void emptyBuffers() {
+    std::fill(buffer_1, buffer_1 + dataPerCharacateristic, 0);
+    std::fill(buffer_2, buffer_2 + dataPerCharacateristic, 0);
+    std::fill(buffer_3, buffer_3 + dataPerCharacateristic, 0);
+    std::fill(buffer_4, buffer_4 + dataPerCharacateristic, 0);
+    std::fill(buffer_5, buffer_5 + dataPerCharacateristic, 0);
+}
+
+void fillCharacteristics() {
+    samples_1.writeValue(buffer_1, 512);
+    samples_2.writeValue(buffer_2, 512);
+    samples_3.writeValue(buffer_3, 512);
+    samples_4.writeValue(buffer_4, 512);
+    samples_5.writeValue(buffer_5, 512);
+}
+
+void fillBuffers(uint32_t& currentSampleBufferIndex, uint32_t& sentData) {
+    uint32_t bufferIndex = currentSampleBufferIndex;
+    uint32_t sent = sentData;
+    uint32_t bytesToSend = eepromGetSampleBufferLength() * 7 - sent;
+    uint32_t bytesInBuffers = 0;
+    uint32_t index = 0;
+
+    
+    if (bytesToSend == 0)
+    {
+        sentData = 0;
+        return;
+    }
+    if (bytesToSend > maxData)
+    {
+        bytesToSend = maxData;
+    }
+    uint32_t samplesToSend = bytesToSend / 7;
+    eepromLockSampleBuffer();
+
+    for (uint32_t i = 0; i < samplesToSend; i++) {
+        SensorSample sample;
+        eepromReadSample(bufferIndex, &sample);
+        
+        
+        if (index == dataPerCharacateristic) 
+        {
+            index = 0;
+        }
+
+        if (bytesInBuffers < dataPerCharacateristic)
+        {
+            addSample(buffer_1, index, sample);
+        }
+
+        else if (bytesInBuffers >= dataPerCharacateristic && bytesInBuffers < dataPerCharacateristic * 2 )
+        {
+            addSample(buffer_2, index, sample);
+        }
+
+        else if (bytesInBuffers >= dataPerCharacateristic * 2 && bytesInBuffers < dataPerCharacateristic * 3 )
+        {
+            addSample(buffer_3, index, sample);
+        }
+
+        else if (bytesInBuffers >= dataPerCharacateristic * 3 && bytesInBuffers < dataPerCharacateristic * 4 )
+        {
+            addSample(buffer_4, index, sample);
+        }
+
+        else
+        {
+            addSample(buffer_5, index, sample);
+        }
+
+        sent += 7;
+        bytesInBuffers += 7;
+        bufferIndex++;
+        if (bufferIndex == 0x4000 / sizeof(SensorSample))
+        {
+            bufferIndex = 0;
+        }
+    }
+    
+    eepromUnlockSampleBuffer();
+    sentData = sent;
+    currentSampleBufferIndex = bufferIndex;
+    
+}
+
+void updateValues() {
+    static uint32_t currentSampleBufferIndex = 0;
+    static uint32_t sentData = 0;
+    if (update.written())
+    {
+    fillBuffers(currentSampleBufferIndex, sentData);
+    fillCharacteristics();
+    emptyBuffers();
+    }
+}
+
+
+
