2f6bfa07ab5a8590f88dc1d9c2d70b745c7f0ee8
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-06 01:30:10 +1300

    Implement lambda for `POST /samples`

--- server/lambda/functions/action-samples-add/index.mjs
+++ server/lambda/functions/action-samples-add/index.mjs
@@ -1,12 +1,39 @@
-// -*- javascript -*-
+import {
+  setupDB,
+} from "/opt/nodejs/lib.mjs";
 
-export const handler = async () => {
-    const response = {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'text/html',
-        },
-        body: html,
-    };
-    return response;
+let db = await setupDB();
+
+console.log("connecting");
+await db.connect();
+console.log("connection success")
+
+export const handler = async (event) => {
+  // NOTE: for now, not checking child IDs
+  // NOTE: for now, not checking payload fields to see if they match column names of DB
+  let sampleMapping = JSON.parse(event.body);
+  let childID;
+  try {
+    await db.query("BEGIN");
+    for (childID in sampleMapping) {
+      let currSamples = sampleMapping[childID];
+      for (let i=0; i<currSamples.length; i++) {
+        await db.query(
+          "INSERT INTO samples (tstamp,child_id,uv_index,lux) VALUES ($1,$2,$3,$4)",
+          [currSamples[i].tstamp,
+           childID,
+           currSamples[i].uv_index,
+           currSamples[i].lux],
+        );
+      }
+      await db.query("COMMIT");
+    }
+  } catch (e) {
+    await db.query("ROLLBACK");
+    throw e;
+  }
+  const response = {
+    statusCode: 200,
+  };
+  return response;
 };
