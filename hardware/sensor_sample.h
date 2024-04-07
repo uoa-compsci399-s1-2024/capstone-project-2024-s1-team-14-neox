e942429713cd6e1348f6cfbd3743bf7ddde1bc73
d-aun <140460323+d-aun@users.noreply.github.com>
2024-04-07 23:08:15 +1200

    Updated to account for returning uint8_t array for timestamp

--- hardware/sensor_sample.h
+++ hardware/sensor_sample.h
@@ -1,8 +1,8 @@
 #ifndef _SENSOR_SAMPLE_H
 #define _SENSOR_SAMPLE_H
 
-#include "rtc.h"
 #include "imu.h"
+#include <array>
 
 /*
  * The uv and light fields store the output of analogRead().
@@ -13,12 +13,12 @@
  */
 
 struct SensorSample {
-  time timestamp;
+  std::array<uint8_t, 4> timestamp;
   uint16_t uv;
   uint16_t light;
   Acceleration acceleration;
 };
 
-static_assert(sizeof(SensorSample) == 12, "SensorSample struct layout assertion failed.");
+static_assert(sizeof(SensorSample) == 14, "SensorSample struct layout assertion failed.");
 
 #endif
