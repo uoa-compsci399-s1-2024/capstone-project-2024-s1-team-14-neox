f7880f66e2f6f683424292b47be626fb75640e97
d-aun <140460323+d-aun@users.noreply.github.com>
2024-04-08 15:51:01 +1200

    Changed bitwise operation

--- hardware/rtc.cpp
+++ hardware/rtc.cpp
@@ -11,10 +11,10 @@ void initializeRTC() {
 
 std::array<uint8_t, 4> readRTC() {
     uint32_t epoch = rtc.getEpoch();
-    uint8_t first_bytes = epoch & 0xff000000;
-    uint8_t second_bytes = epoch & 0x00ff0000 << 8;
-    uint8_t third_bytes = epoch & 0x0000ff00 << 16;
-    uint8_t fourth_bytes = epoch & 0x000000ff << 24;
+    uint8_t first_bytes = epoch & 0x00ff;
+    uint8_t second_bytes = epoch >> 8;
+    uint8_t third_bytes = epoch >> 16;
+    uint8_t fourth_bytes = epoch >> 24;
     std::array<uint8_t, 4> output = {first_bytes, second_bytes, third_bytes, fourth_bytes};
     return output;
 }
