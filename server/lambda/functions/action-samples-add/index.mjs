78506cc2a69f74dd4799d4c4236e814bb9e0a820
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-14 10:40:07 +1200

    Set up path parameter inputs to sample adding action

--- server/lambda/functions/action-samples-add/index.mjs
+++ server/lambda/functions/action-samples-add/index.mjs
@@ -8,23 +8,20 @@ let db = await connectToDB();
 export const handler = async (event) => {
   // NOTE: for now, not checking child IDs
   // NOTE: for now, not checking payload fields to see if they match column names of DB
-  let sampleMapping = JSON.parse(event.body);
-  let childID;
+  let samples = JSON.parse(event.body).samples;
+  let childID = event.pathParameters.childID;
   try {
     await db.query("BEGIN");
-    for (childID in sampleMapping) {
-      let currSamples = sampleMapping[childID];
-      for (let i=0; i<currSamples.length; i++) {
-        await db.query(
-          "INSERT INTO samples (tstamp,child_id,uv_index,lux) VALUES ($1,$2,$3,$4)",
-          [currSamples[i].tstamp,
-           childID,
-           currSamples[i].uv_index,
-           currSamples[i].lux],
-        );
-      }
-      await db.query("COMMIT");
+    for (let i=0; i<samples.length; i++) {
+      await db.query(
+        "INSERT INTO samples (tstamp,child_id,uv_index,lux) VALUES ($1,$2,$3,$4)",
+        [samples[i].tstamp,
+         childID,
+         samples[i].uv_index,
+         samples[i].lux],
+      );
     }
+    await db.query("COMMIT");
   } catch (e) {
     await db.query("ROLLBACK");
     throw e;
