5bd3733e2c367288eb3ee0f05422c14bf74351dc
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-14 21:09:25 +1200

    Add some logging to add samples action

--- server/lambda/functions/action-samples-add/index.mjs
+++ server/lambda/functions/action-samples-add/index.mjs
@@ -10,8 +10,11 @@ export const handler = async (event) => {
   // NOTE: for now, not checking payload fields to see if they match column names of DB
   let samples = JSON.parse(event.body).samples;
   let childID = event.pathParameters.childID;
+  console.log(`childID=${childID}`);
+  console.log(`request body: ${event.body}`);
   for (let i=0; i<samples.length; i++) {
     try {
+      console.log(`adding sample for child ID "${childID}" with timestamp "${samples[i].tstamp}": {uv: "${samples[i].uv_index}", light: "${samples[i].lux}"}`);
       await db.query(
         "INSERT INTO samples (tstamp,child_id,uv_index,lux) VALUES ($1,$2,$3,$4)",
         [samples[i].tstamp,
