eb3f99bc544d0bc410a2e7679d8016db723c79c7
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-19 21:01:18 +1200

    Set up path parameter input for add-sample

--- server/lambda/functions/action-samples-add/index.mjs
+++ server/lambda/functions/action-samples-add/index.mjs
@@ -52,86 +52,83 @@ export const handler = async (event) => {
     addCorsHeaders(errorResp);
     return errorResp;
   }
-  let childID;
+  const childID = event.pathParameters.childID;
+  const resolvedResource = event.resource.replace("{childID}", childID);
   let errors = [];
-  for (childID in sampleMapping) {
-    let resolvedResource = `${event.resource}/${childID}`;
-    let currSamples = sampleMapping[childID];
-    for (let i=0; i<currSamples.length; i++) {
-      let missingfields = false;
-      for (let j=0; j<REQUIRED_FIELDS.length; j++) {
-        if (currSamples[i][REQUIRED_FIELDS[j]] == null) {
-          errors.push({
-            resource: `${resolvedResource}?index=${i}&field=${REQUIRED_FIELDS[j]}`,
-            status: 400,
-            message: `${REQUIRED_FIELDS[j]} missing from sample`,
-          });
-          console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
-          missingfields = true;
-          continue;
-        }
-        if (currSamples[i].child_id !== undefined && currSamples[i].child_id !== childID) {
-          errors.push({
-            resource: `${resolvedResource}?index=${i}&field=child_id`,
-            status: 400,
-            message: `child IDs don't match in sample (${currSamples[i].child_id}) and in containing object (${childID})`,
-          });
-          console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
-          missingfields = true;
-          continue;
-        }
+  for (let i=0; i<samples.length; i++) {
+    let missingfields = false;
+    for (let j=0; j<REQUIRED_FIELDS.length; j++) {
+      if (samples[i][REQUIRED_FIELDS[j]] == null) {
+        errors.push({
+          resource: `${resolvedResource}?index=${i}&field=${REQUIRED_FIELDS[j]}`,
+          status: 400,
+          message: `${REQUIRED_FIELDS[j]} missing from sample`,
+        });
+        console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
+        missingfields = true;
+        continue;
       }
-      if (missingfields) {
-        continue;  // to next sample
+      if (samples[i].child_id !== undefined && samples[i].child_id !== childID) {
+        errors.push({
+          resource: `${resolvedResource}?index=${i}&field=child_id`,
+          status: 400,
+          message: `child IDs don't match in sample (${samples[i].child_id}) and in path (${childID})`,
+        });
+        console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
+        missingfields = true;
+        continue;
       }
+    }
+    if (missingfields) {
+      continue;  // to next sample
+    }
 
-      if (!isMatch(currSamples[i].timestamp, DATETIME_FORMAT_UTC) ||
-          !isMatch(currSamples[i].timestamp, DATETIME_FORMAT_WITHOFFSET)) {
+    if (!isMatch(samples[i].timestamp, DATETIME_FORMAT_UTC) ||
+        !isMatch(samples[i].timestamp, DATETIME_FORMAT_WITHOFFSET)) {
+      errors.push({
+        resource: `${resolvedResource}?index=${i}&field=timestamp`,
+        status: 400,
+        message: `timestamp must be in full ISO8601 datetime format with offset OR in UTC`,
+      });
+      console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
+      continue;
+    }
+
+    // structuredClone is new to JS but available in Node
+    let logSample = structuredClone(samples[i]);
+    delete logSample.child_id;  // if present
+    delete logSample.timestamp;
+    console.log(`adding sample for child ID "${childID}" with timestamp "${samples[i].timestamp}": ${JSON.stringify(logSample)}`);
+    // TODO check if child exists
+    // Don't need to add BEGIN and COMMIT (plus ROLLBACK) statements because this is atomic.
+    try {
+      await db.query(
+        "INSERT INTO samples (\"timestamp\",child_id,uv,light) VALUES ($1,$2,$3,$4)",
+        [samples[i].timestamp,
+         childID,
+         samples[i].uv,
+         samples[i].light],
+      );
+    } catch (e) {
+      if (e.code === UNIQUE_VIOLATION && e.constraint === "samples_pkey") {
         errors.push({
           resource: `${resolvedResource}?index=${i}&field=timestamp`,
-          status: 400,
-          message: `timestamp must be in full ISO8601 datetime format with offset OR in UTC`,
+          status: 409,
+          message: `sample timestamp already seen`,
         });
         console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
         continue;
-      }
-
-      // structuredClone is new to JS but available in Node
-      let logSample = structuredClone(currSamples[i]);
-      delete logSample.child_id;  // if present
-      delete logSample.timestamp;
-      console.log(`adding sample for child ID "${childID}" with timestamp "${currSamples[i].timestamp}": ${JSON.stringify(logSample)}`);
-      // TODO check if child exists
-      // Don't need to add BEGIN and COMMIT (plus ROLLBACK) statements because this is atomic.
-      try {
-        await db.query(
-          "INSERT INTO samples (\"timestamp\",child_id,uv,light) VALUES ($1,$2,$3,$4)",
-          [currSamples[i].timestamp,
-           childID,
-           currSamples[i].uv,
-           currSamples[i].light],
-        );
-      } catch (e) {
-        if (e.code === UNIQUE_VIOLATION && e.constraint === "samples_pkey") {
-          errors.push({
-            resource: `${resolvedResource}?index=${i}&field=timestamp`,
-            status: 409,
-            message: `sample timestamp already seen`,
-          });
-          console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
-          continue;
-        } else if (e.code === FOREIGN_KEY_VIOLATION && e.constraint === "samples_child_id_fkey") {
-          // FIXME: Handle permissions.
-          errors.push({
-            resource: `${resolvedResource}?index=${i}&field=child_id`,
-            status: 403,
-            message: `child ID doesn't exist or user is not authorised to add samples to the child`,
-          });
-          console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
-          continue;
-        } else {
-          throw e;
-        }
+      } else if (e.code === FOREIGN_KEY_VIOLATION && e.constraint === "samples_child_id_fkey") {
+        // FIXME: Handle permissions.
+        errors.push({
+          resource: `${resolvedResource}?index=${i}&field=child_id`,
+          status: 403,
+          message: `child ID doesn't exist or user is not authorised to add samples to the child`,
+        });
+        console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
+        continue;
+      } else {
+        throw e;
       }
     }
   }
