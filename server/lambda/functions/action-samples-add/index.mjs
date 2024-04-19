d40317051a1fcde6e413ed65776a4deb7d11e753
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-19 13:27:06 +1200

    Remove useless DB transaction statements

--- server/lambda/functions/action-samples-add/index.mjs
+++ server/lambda/functions/action-samples-add/index.mjs
@@ -27,39 +27,39 @@ export const handler = async (event) => {
   let sampleMapping = JSON.parse(event.body);
   let childID;
   let errors = [];
-  try {
-    await db.query("BEGIN");
-    for (childID in sampleMapping) {
-      let resolvedResource = `${event.resource}/${childID}`;
-      let currSamples = sampleMapping[childID];
-      for (let i=0; i<currSamples.length; i++) {
-        for (let j=0; j<REQUIRED_FIELDS.length; j++) {
-          if (currSamples[i][REQUIRED_FIELDS[j]] == null) {
-            errors.push({
-              resource: `${resolvedResource}?index=${i}&field=${REQUIRED_FIELDS[j]}`,
-              status: 400,
-              message: `${REQUIRED_FIELDS[j]} missing from sample`,
-            });
-            console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
-            continue;
-          }
-          if (currSamples[i].child_id !== undefined && currSamples[i].child_id !== childID) {
-            errors.push({
-              resource: `${resolvedResource}?index=${i}&field=child_id`,
-              status: 400,
-              message: `child IDs don't match in sample (${currSamples[i].child_id}) and in containing object (${childID})`,
-            });
-            console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
-            continue;
-          }
+  for (childID in sampleMapping) {
+    let resolvedResource = `${event.resource}/${childID}`;
+    let currSamples = sampleMapping[childID];
+    for (let i=0; i<currSamples.length; i++) {
+      for (let j=0; j<REQUIRED_FIELDS.length; j++) {
+        if (currSamples[i][REQUIRED_FIELDS[j]] == null) {
+          errors.push({
+            resource: `${resolvedResource}?index=${i}&field=${REQUIRED_FIELDS[j]}`,
+            status: 400,
+            message: `${REQUIRED_FIELDS[j]} missing from sample`,
+          });
+          console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
+          continue;
         }
+        if (currSamples[i].child_id !== undefined && currSamples[i].child_id !== childID) {
+          errors.push({
+            resource: `${resolvedResource}?index=${i}&field=child_id`,
+            status: 400,
+            message: `child IDs don't match in sample (${currSamples[i].child_id}) and in containing object (${childID})`,
+          });
+          console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
+          continue;
+        }
+      }
 
-        // structuredClone is new to JS but available in Node
-        let logSample = structuredClone(currSamples[i]);
-        delete logSample.child_id;  // if present
-        delete logSample.timestamp;
-        console.log(`adding sample for child ID "${childID}" with timestamp "${currSamples[i].timestamp}": ${JSON.stringify(logSample)}`);
-        // TODO check if child exists
+      // structuredClone is new to JS but available in Node
+      let logSample = structuredClone(currSamples[i]);
+      delete logSample.child_id;  // if present
+      delete logSample.timestamp;
+      console.log(`adding sample for child ID "${childID}" with timestamp "${currSamples[i].timestamp}": ${JSON.stringify(logSample)}`);
+      // TODO check if child exists
+      // Don't need to add BEGIN and COMMIT (plus ROLLBACK) statements because this is atomic.
+      try {
         await db.query(
           "INSERT INTO samples (\"timestamp\",child_id,uv,light) VALUES ($1,$2,$3,$4)",
           [currSamples[i].timestamp,
@@ -67,12 +67,10 @@ export const handler = async (event) => {
            currSamples[i].uv,
            currSamples[i].light],
         );
+      } catch (e) {
+        throw e;
       }
-      await db.query("COMMIT");
     }
-  } catch (e) {
-    await db.query("ROLLBACK");
-    throw e;
   }
 
   const response = {};
