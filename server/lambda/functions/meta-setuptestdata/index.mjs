8bd1e80c27d2da5a65692dc937e90660b3b4f77f
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-20 00:47:19 +1200

    server: Rename sample fields to their API names, move childID to path parameter,  handle errors for add-sample action (#45)
    The sample search action also outputs ISO-8601 datetimes (with timezone) with seconds resolution.
    
    Here are the checks that the sample adding action does:
    
    - The `Content-Type` header is `application/json`
    - Request body:
      - is present,
      - parses as JSON, and
      - has a `samples` property which is an array containing samples
    - If `child_id` field is in a sample, then the ID in the sample (eg `"child_id": "22"`) and in the path parameter (eg `/samples/22`) agree
    - Child ID must refer to an existing child (no permissions checking because I haven't implemented users yet)
    - Samples have the following fields:
      - `timestamp`
      - `uv`
      - `light`
    - Timestamps are ISO-8601 datetimes with a timezone using either `Z` or `+00:00` for UTC 0
    - Sample timestamps must be unique for a given child
    - `uv` and `light` are non-negative integers
    
    Squashed commits:
    
    * Rename DB column names for timestamp, uv, and light to match API spec
    
    * Handle DB errors in sample search action
    
    * Add function to validate Content-Type headers
    
    * Validate Content-Type header when adding samples
    
    * add-samples: Check if all required fields exist
    
    * Remove useless DB transaction statements
    
    * LayerJS: Install pg-error-constants
    
    * LayerJS: Install date-fns for datetime handling
    
    * Validate sample timestamps
    
    * Fix undefined errors
    
    * Quote content-type in error messages
    
    * Shorten code to set constants for datetime formats
    
    * Move datetime format constants in lib
    
    * Set the output formatting of sample timestamps from API
    
    * Fix syntax
    
    * Validate request body is non-empty
    
    * Fix required field name
    
    * Skip to next sample if missing any fields
    
    * Remove outdated comment
    
    * Catch duplicate timestamp errors
    
    * Make composite primary key for samples
    
    * Catch samples for unknown child IDs
    
    * fixup duplicate sample timestamps
    
    * Print errors when adding sample to DB
    
    * Set up path parameter input for add-sample
    
    * Remove outdated comment
    
    * Move child ID agreement checking out of field checking loop
    
    * Resolve event resource
    
    * Check as many fields as possible so caller can fix more errors earlier
    
    * Check request body against schema
    
    * Initialise an object for early errors to use for early return
    
    This file is getting pretty big with all this error handling now.
    
    * Properly declare `samples` variable
    
    * Stop loop early when DB throws no-such-child error
    
    * Check missing or empty request bodies even if JSON.parse succeeds
    
    * Make uv and light DB columns non-null
    
    Kevin said we can assume samples will always have every field.
    
    * Handle non-integer values for uv and light
    
    * Handle values out of range for uv and light

--- server/lambda/functions/meta-setuptestdata/index.mjs
+++ server/lambda/functions/meta-setuptestdata/index.mjs
@@ -7,5 +7,5 @@ let db = await connectToDB();
 export const handler = async (event) => {
   await db.query("INSERT INTO parents (id, fname, lname) VALUES ('1', 'John', 'Cena')");
   await db.query("INSERT INTO children (id, parent_id, fname) VALUES ('22', '1', 'Bobby')");
-  await db.query("INSERT INTO samples (child_id, tstamp, uv_index, lux) VALUES ('22', '2024-02-01+12', 2, 1500)");
+  await db.query("INSERT INTO samples (child_id, \"timestamp\", uv, light) VALUES ('22', '2024-02-01+12', 2, 1500)");
 };
