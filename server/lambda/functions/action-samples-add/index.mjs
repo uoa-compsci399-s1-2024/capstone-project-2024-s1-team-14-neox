5420027e4fdaee8032c8f2096233fbc7ec0be105
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-19 13:23:02 +1200

    add-samples: Check if all required fields exist

--- server/lambda/functions/action-samples-add/index.mjs
+++ server/lambda/functions/action-samples-add/index.mjs
@@ -6,6 +6,12 @@ import {
 
 let db = await connectToDB();
 
+const REQUIRED_FIELDS = [
+  "timestamp".
+  "uv",
+  "light,"
+];
+
 export const handler = async (event) => {
   const contentTypeError = validateContentType(event.headers, event.resource);
   if (contentTypeError !== null) {
@@ -20,12 +26,40 @@ export const handler = async (event) => {
   // NOTE: for now, not checking payload fields to see if they match column names of DB
   let sampleMapping = JSON.parse(event.body);
   let childID;
+  let errors = [];
   try {
     await db.query("BEGIN");
     for (childID in sampleMapping) {
+      let resolvedResource = `${event.resource}/${childID}`;
       let currSamples = sampleMapping[childID];
       for (let i=0; i<currSamples.length; i++) {
-        console.log(`adding sample for child ID "${childID}" with timestamp "${currSamples[i].timestamp}": {uv: "${currSamples[i].uv}", light: "${currSamples[i].light}"}`);
+        for (let j=0; j<REQUIRED_FIELDS.length; j++) {
+          if (currSamples[i][REQUIRED_FIELDS[j]] == null) {
+            errors.push({
+              resource: `${resolvedResource}?index=${i}&field=${REQUIRED_FIELDS[j]}`,
+              status: 400,
+              message: `${REQUIRED_FIELDS[j]} missing from sample`,
+            });
+            console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
+            continue;
+          }
+          if (currSamples[i].child_id !== undefined && currSamples[i].child_id !== childID) {
+            errors.push({
+              resource: `${resolvedResource}?index=${i}&field=child_id`,
+              status: 400,
+              message: `child IDs don't match in sample (${currSamples[i].child_id}) and in containing object (${childID})`,
+            });
+            console.error(`${childID}:index=${i}: ${errors[errors.length-1].message}`);
+            continue;
+          }
+        }
+
+        // structuredClone is new to JS but available in Node
+        let logSample = structuredClone(currSamples[i]);
+        delete logSample.child_id;  // if present
+        delete logSample.timestamp;
+        console.log(`adding sample for child ID "${childID}" with timestamp "${currSamples[i].timestamp}": ${JSON.stringify(logSample)}`);
+        // TODO check if child exists
         await db.query(
           "INSERT INTO samples (\"timestamp\",child_id,uv,light) VALUES ($1,$2,$3,$4)",
           [currSamples[i].timestamp,
@@ -40,9 +74,16 @@ export const handler = async (event) => {
     await db.query("ROLLBACK");
     throw e;
   }
-  const response = {
-    statusCode: 200,
-  };
+
+  const response = {};
+  if (errors.length > 0) {
+    response.statusCode = 207;
+    response.body = JSON.stringify({
+      "errors": errors,
+    });
+  } else {
+    response.statusCode = 204;
+  }
   addCorsHeaders(response);
   return response;
 };
