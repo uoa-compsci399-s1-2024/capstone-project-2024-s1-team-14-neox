89fc31b70dbc1b70ec6992ddd7d55d98c1ea4a6d
hsir571 <79824690+hsir571@users.noreply.github.com>
2024-04-22 17:42:51 +1200

    Copy Paste Authetnication stuff
    Not mine just some copy paste stuff to look like theres something there

--- app/lib/bluetooth/presentation/tiles/system_device_tile.dart
+++ app/lib/bluetooth/presentation/tiles/system_device_tile.dart
@@ -137,9 +137,9 @@ class _SystemDeviceTileState extends State<SystemDeviceTile> {
       //   if (adv.advName.isNotEmpty) _buildAdvRow(context, 'Name', adv.advName),
       //   if (adv.txPowerLevel != null) _buildAdvRow(context, 'Tx Power Level', '${adv.txPowerLevel}'),
       //   if ((adv.appearance ?? 0) > 0) _buildAdvRow(context, 'Appearance', '0x${adv.appearance!.toRadixString(16)}'),
-      //   if (adv.msd.isNotEmpty) _buildAdvRow(context, 'Manufacturer Data', getNiceManufacturerData(adv.msd)),
+      //   if (adv.msd.isNotEmpty) _buildAdvRow(context, 'Manufacturer data', getNiceManufacturerData(adv.msd)),
       //   if (adv.serviceUuids.isNotEmpty) _buildAdvRow(context, 'Service UUIDs', getNiceServiceUuids(adv.serviceUuids)),
-      //   if (adv.serviceData.isNotEmpty) _buildAdvRow(context, 'Service Data', getNiceServiceData(adv.serviceData)),
+      //   if (adv.serviceData.isNotEmpty) _buildAdvRow(context, 'Service data', getNiceServiceData(adv.serviceData)),
       // ],
     );
   }
--- app/lib/cloud/presentation/screen/login_screen.dart
+++ app/lib/cloud/presentation/screen/login_screen.dart
@@ -1,37 +1,99 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../cubit/login_cubit.dart';
-
+import '../../data/services/aws/aws_cognito.dart'; // Import your AWS Cognito service
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    late TextEditingController emailController;
+    late TextEditingController passwordController;
+
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Connect with Neox Cloud"),
       ),
-      body: Column(
-        children: [
-          ElevatedButton.icon(
-            label: const Text(
-              'SIGN IN WITH GOOGLE',
-              style: TextStyle(color: Colors.white),
-            ),
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
               ),
-              backgroundColor: Theme.of(context).colorScheme.secondary,
+              keyboardType: TextInputType.emailAddress,
             ),
-            icon: const Icon(FontAwesomeIcons.google, color: Colors.white),
-            onPressed: () => context.read<LoginCubit>().logInWithGoogle(),
-          ),
-        ],
+            const SizedBox(height: 20),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  // Call createInitialRecord method from AWSServices to attempt to create a user
+                  await AWSServices().createInitialRecord(
+                    emailController.text,
+                    passwordController.text,
+                  );
+                } catch (e) {
+                  // Handle any errors that occur during user creation
+                  print('User creation error: $e');
+                  
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('User creation failed: $e'),
+                      duration: const Duration(seconds: 3),
+                    ),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  'SIGN IN',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton.icon(
+              label: const Text(
+                'SIGN IN WITH GOOGLE',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+              ),
+              icon: const Icon(FontAwesomeIcons.google, color: Colors.white),
+              onPressed: () {
+                // Handle Google sign-in
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
