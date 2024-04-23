5b448c07a979946487bffe18fe1ea8733202610a
Hoboneer <gabriel.lisaca@gmail.com>
2024-04-23 23:09:59 +1200

    server: Return DB output from FuncMetaReadAllTables
    Rather than logging it since too many samples truncated the log.

--- server/lambda/functions/meta-read-all-tables/index.mjs
+++ server/lambda/functions/meta-read-all-tables/index.mjs
@@ -8,10 +8,9 @@ export const handler = async (event) => {
   const res_parents = await db.query("SELECT * FROM parents");
   const res_children = await db.query("SELECT * FROM children");
   const res_samples = await db.query("SELECT * FROM samples");
-  console.log("parents:");
-  console.dir(res_parents.rows);
-  console.log("children:");
-  console.dir(res_children.rows);
-  console.log("samples:");
-  console.dir(res_samples.rows);
+  return {
+    parents: res_parents.rows,
+    children: res_children.rows,
+    samples: res_samples.rows,
+  };
 };
