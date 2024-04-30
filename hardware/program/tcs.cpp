2afa7ba9e7311d7c580eac0fd30ce9ad9af37d2c
d-aun <david.auna8@gmail.com>
2024-04-30 21:05:18 +1200

    Added code for colour sensor

---
+++ hardware/program/tcs.cpp
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
+#include "tcs.h"
+
+Adafruit_TCS34725 tcs(101);
+
+void initializeTCS() {
+  if (!tcs.begin())
+  {
+    Serial.print("TCS not found ... Check connection");
+    while (1);
+  }
+}
+
+std::array<uint16_t, 5> readTCS() {
+  uint16_t r, g, b, c, colorTemp, lux;
+
+  tcs.getRawData(&r, &g, &b, &c);
+  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c); 
+  lux = tcs.calculateLux(r, g, b);
+
+  std::array<uint16_t, 5> output = {r, g, b, colorTemp, lux};
+  return output;
+}
