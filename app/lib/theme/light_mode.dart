8d531cfb13a11f16664ac129f35cd3b8a150f53d
hsir571 <79824690+hsir571@users.noreply.github.com>
2024-05-01 01:00:42 +1200

    App: Implement dark mode (#62)
    * Dark mode
    
    * App: add newline to end of files
    
    ---------
    
    Co-authored-by: luciaskim <97372163+luciaskim@users.noreply.github.com>

---
+++ app/lib/theme/light_mode.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background:  Colors.white,
+    primary: Colors.purple.shade500,
+    secondary: Colors.purple.shade200,
+    tertiary: Colors.grey.shade200,
+    inversePrimary: Colors.purple.shade900,
+  ),
+);
