7f32ebeb8e17b13e6a4b88491514a3d835c65da1
scho815 <161417533+scho815@users.noreply.github.com>
2024-05-12 02:22:12 +1200

    Website: add popup box, signout and redirect unauthorised users

--- website/src/App.js
+++ website/src/App.js
@@ -2,14 +2,16 @@
 //import button from './data/button.png';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
-import Login from './data/login'
-import Users from './data/users'
-import Home from './data/home'
-import Create from './data/create'
-import PrivateRoutes from './data/privateRoutes'
-import DisplayChart from './data/displayChart'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import Login from './data/login';
+import Users from './data/users';
+import Popup from './popup';
+import AccessDenied from './data/accessDenied';
+import Home from './data/home';
+import Create from './data/create';
+import PrivateRoutes from './data/privateRoutes';
+import DisplayChart from './data/displayChart';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import logo from './data/neox.svg';
 
 //Auth related imports
@@ -34,70 +36,74 @@ function App() {
     setShowButton(value !== undefined ? value : !showButton);
   };
 
-  const toggleAdminContent = (value) => {
-    setIsAdmin(value !== undefined ? value : !isAdmin);
-  };
-  
-  const [jwtToken, setJwtToken] = useState(null); // State to store jwtToken
+  //trigger for popup box
+  const [isOpen, setIsOpen] = useState(false);
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+  // State to store jwtToken
+  const [jwtToken, setJwtToken] = useState(null); 
   const handleJwtToken = (token) => {
     setJwtToken(token);
   };
 
-const handleSignOut = async () => {
-  try {
-    await Auth.signOut();
-    console.log('Sign-out successful.');
-    window.location.href = '/';
-    toggleButton(true);
-  } catch (error) {
-    console.error('Error signing out:', error);
-  }
-};
 
-useEffect(() => {
-  // Clear authentication tokens upon component mount
-  Amplify.Auth.signOut();
-}, []);
 
-const listener = (data) => {
-  switch (data?.payload?.event) {
-    case 'signIn':
-      toggleButton(false);
-      fetchJwtToken();
-      console.log('user signed in');
-
-      break;
-    case 'signUp':
-      //toggleButton(false);
-      console.log('user signed up');
-      break;
-    case 'signOut':
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut();
+      console.log('Sign-out successful.');
+      window.location.href = '/';
       toggleButton(true);
-      console.log('user signed out');
-      break;
-  }
-};
-
-Hub.listen('auth', listener);
-
-
-const fetchJwtToken = async () => {
-  try {
-    const currentUser = await Auth.currentAuthenticatedUser();
-    const session = await Auth.currentSession();
-    const token = session.getIdToken().getJwtToken();
-    setJwtToken(token);
-    handleJwtToken(token); // Pass jwtToken back to App.js
-    console.log("user belongs to following groups: " + session.getIdToken().payload["cognito:groups"]);
-    const groups = session.getIdToken().payload["cognito:groups"];
-    if (groups && groups.includes("admins")) {
-      setIsAdmin(true);
+    } catch (error) {
+      console.error('Error signing out:', error);
     }
+  };
+  
+  useEffect(() => {
+    // Clear authentication tokens upon component mount
+    Amplify.Auth.signOut();
+  }, []);
 
-  } catch (error) {
-    console.log('Error fetching JWT token:', error);
-  }
-};
+  const listener = (data) => {
+    switch (data?.payload?.event) {
+      case 'signIn':
+        toggleButton(false);
+        fetchJwtToken();
+        console.log('user signed in');
+        break;
+      case 'signUp':
+        console.log('user signed up');
+        break;
+      case 'signOut':
+        toggleButton(true);
+        console.log('user signed out');
+        break;
+    }
+  };
+
+  Hub.listen('auth', listener);
+
+  const fetchJwtToken = async () => {
+    try {
+      const currentUser = await Auth.currentAuthenticatedUser();
+      const session = await Auth.currentSession();
+      const token = session.getIdToken().getJwtToken();
+      setJwtToken(token);
+      handleJwtToken(token); // Pass jwtToken back to App.js
+      console.log("user belongs to following groups: " + session.getIdToken().payload["cognito:groups"]);
+      const groups = session.getIdToken().payload["cognito:groups"];
+      if (groups && groups.includes("admins")) {
+        setIsAdmin(true);
+      }else if (groups && groups.includes("researchers")){
+        
+      }else{
+        //handleUnauthorisedSignOut();
+      }
+    } catch (error) {
+      console.log('Error fetching JWT token:', error);
+    }
+  };
 
   return (
     <div className="Background">
@@ -106,7 +112,8 @@ const fetchJwtToken = async () => {
         <div className="App">
           <nav className="navbar navbar-expand-lg navbar-light fixed-top">
             <div className="container">
-              <Link to={'/'} className="navbar-brand"><img src={logo} alt="NEOX Logo" width="140px" /></Link>
+            {showButton && <Link to={'/'} className="navbar-brand"><img src={logo} alt="NEOX Logo" width="140px" /></Link>}
+            {!showButton && <Link to={'/home'} className="navbar-brand"><img src={logo} alt="NEOX Logo" width="140px" /></Link>}
                 <ul className="nav">
                   {showButton &&<li className="nav-item"><Link className="nav-link" to={'/'}>Login</Link></li>}
                   {!showButton && isAdmin && <li className="nav-item"><Link className="nav-link" to={'/users'}>Users</Link></li>}
@@ -133,13 +140,13 @@ const fetchJwtToken = async () => {
                 {!showButton && (
                   <Route path="/home" element={<Home isAdmin={isAdmin} showButton={showButton} />} />
                 )}
+                <Route path="/accessDenied" element={<AccessDenied/>} />
               </Routes>
             </div>
           </div>
 
         </div>
       </Router>
-      
     </div>  
   );
 }
