9a9abdd76da3b41b2a055e49d42c8a4c44185a87
hsir571 <79824690+hsir571@users.noreply.github.com>
2024-05-13 13:21:42 +1200

    Update study_entity.dart
    Add Queries

--- app/lib/data/entities/study_entity.dart
+++ app/lib/data/entities/study_entity.dart
@@ -1,3 +1,4 @@
+import 'package:capstone_project_2024_s1_team_14_neox/data/entities/study_entity.dart';
 import 'package:drift/drift.dart';
 import 'package:capstone_project_2024_s1_team_14_neox/data/dB/database.dart';
 
@@ -28,8 +29,55 @@ class StudyEntity {
   StudyEntity({this.id, required this.name, required this.description, required this.startDate,
   required this.endDate, required this.studyCode});
 
-  // StudyCompanion toCompanion() {
-  //
-  // }
+   StudyCompanion toCompanion() {
+    return StudyCompanion(
+      name: Value(name),
+      description: Value(description),
+      startDate: Value(startDate),
+      endDate: Value(endDate),
+      studyCode: Value(studyCode)
+    );
+   }
+
+
+// QUERIES
+
+///////////////////////////////////////////////////////////////
+//   // CREATE ///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+  static Future<void> createStudy(StudyEntity Study) async {
+    AppDb db = AppDb.instance();
+    await db
+        .into(db.study)
+        .insert(Study.toCompanion(), mode: InsertMode.insert);
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+  // READ ////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
+  static Future<StudyEntity?> queryStudyByCode(String studyCode) async {
+    AppDb db = AppDb.instance();
+    StudyEntity? child = await (db.select(db.study)
+      ..where((tbl) => tbl.studyCode.equals(studyCode)))
+        .getSingleOrNull();
+
+    return child;
+  }
+
+  static Future<List<StudyEntity>> queryAllStudy() async {
+    AppDb db = AppDb.instance();
+    List<StudyEntity> studyList = await db.select(db.study).get();
+    return studyList;
+  }
+
+////////////////////////////////////////////////////////////////////////////
+// DELETE //////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+  static Future<void> deleteStudy(String studyCode) async {
+    AppDb db = AppDb.instance();
+    await (db.delete(db.study)..where((tbl) => tbl.studyCode.equals(studyCode))).go();
+  }
 
 }
