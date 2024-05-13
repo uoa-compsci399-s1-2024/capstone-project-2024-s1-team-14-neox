28363a760bdfccf25421cfbca733460f0fc5a373
scho815 <161417533+scho815@users.noreply.github.com>
2024-05-13 17:18:48 +1200

    Website: added and updated role based access, login, home, users and … (#73)
    * Website: added and updated role based access, login, home, users and create pages
    
    now admins and researchers have different options on the website
    
    * Website: added and updated role based access, login, home, users and …
    
    * Website: added and updated role based access, login, home, users and …
    
    too lazy to compare changes.
    
    * Website: add popup box, signout and redirect unauthorised users
    
    * Website: add popup box, signout and redirect unauthorised users
    
    * Website: removed unused dependency
    
    * Website: add popup box. signout and redirect unauthorised users
    
    * Website: Just double checking to see if the change is applied
    
    * Website : add response messages for /users, page for unauthorised login.
    
    admin receives messages for successful/failed account creation in /users

--- website/src/App.js
+++ website/src/App.js
@@ -2,12 +2,16 @@
 //import button from './data/button.png';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
-import Login from './data/login'
-import Home from './data/home'
-import PrivateRoutes from './data/privateRoutes'
-import DisplayChart from './data/displayChart'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import Login from './data/login';
+import Users from './data/users';
+import Popup from './popup';
+import AccessDenied from './data/accessDenied';
+import Home from './data/home';
+import Create from './data/create';
+import PrivateRoutes from './data/privateRoutes';
+import DisplayChart from './data/displayChart';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import logo from './data/neox.svg';
 
 //Auth related imports
@@ -24,52 +28,82 @@ Amplify.configure({
 });
 
 function App() {
-
+  // hides and shows contents
   const [showButton, setShowButton] = useState(true);  
+  // hides and shows admin contents
+  const [isAdmin, setIsAdmin] = useState(false);
   const toggleButton = (value) => {
     setShowButton(value !== undefined ? value : !showButton);
   };
-  
-  const [jwtToken, setJwtToken] = useState(null); // State to store jwtToken
+
+  //trigger for popup box
+  const [isOpen, setIsOpen] = useState(false);
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+  // State to store jwtToken
+  const [jwtToken, setJwtToken] = useState(null); 
   const handleJwtToken = (token) => {
     setJwtToken(token);
   };
 
-const handleSignOut = async () => {
-  try {
-    await Auth.signOut();
-    console.log('Sign-out successful.');
-    window.location.href = '/sign-in';
-    toggleButton(true);
-  } catch (error) {
-    console.error('Error signing out:', error);
-  }
-};
 
-useEffect(() => {
-  // Clear authentication tokens upon component mount
-  Amplify.Auth.signOut();
-}, []);
 
-  
-const listener = (data) => {
-  switch (data?.payload?.event) {
-    case 'signIn':
-      toggleButton(false);
-      console.log('user signed in');
-      break;
-    case 'signUp':
-      //toggleButton(false);
-      console.log('user signed up');
-      break;
-    case 'signOut':
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut();
+      console.log('Sign-out successful.');
+      window.location.href = '/';
       toggleButton(true);
-      console.log('user signed out');
-      break;
-  }
-};
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+  
+  useEffect(() => {
+    // Clear authentication tokens upon component mount
+    Amplify.Auth.signOut();
+  }, []);
 
-Hub.listen('auth', listener);
+  const listener = (data) => {
+    switch (data?.payload?.event) {
+      case 'signIn':
+        toggleButton(false);
+        fetchJwtToken();
+        console.log('user signed in');
+        break;
+      case 'signUp':
+        console.log('user signed up');
+        break;
+      case 'signOut':
+        toggleButton(true);
+        console.log('user signed out');
+        break;
+    }
+  };
+
+  Hub.listen('auth', listener);
+
+  const fetchJwtToken = async () => {
+    try {
+      const currentUser = await Auth.currentAuthenticatedUser();
+      const session = await Auth.currentSession();
+      const token = session.getIdToken().getJwtToken();
+      setJwtToken(token);
+      handleJwtToken(token); // Pass jwtToken back to App.js
+      console.log("user belongs to following groups: " + session.getIdToken().payload["cognito:groups"]);
+      const groups = session.getIdToken().payload["cognito:groups"];
+      if (groups && groups.includes("admins")) {
+        setIsAdmin(true);
+      }else if (groups && groups.includes("researchers")){
+        
+      }else{
+        //handleUnauthorisedSignOut();
+      }
+    } catch (error) {
+      console.log('Error fetching JWT token:', error);
+    }
+  };
 
   return (
     <div className="Background">
@@ -78,11 +112,14 @@ Hub.listen('auth', listener);
         <div className="App">
           <nav className="navbar navbar-expand-lg navbar-light fixed-top">
             <div className="container">
-              <Link to={'/Home'} className="navbar-brand">   <img src={logo} alt="NEOX Logo" width="140px" /></Link>
+            {showButton && <Link to={'/'} className="navbar-brand"><img src={logo} alt="NEOX Logo" width="140px" /></Link>}
+            {!showButton && <Link to={'/home'} className="navbar-brand"><img src={logo} alt="NEOX Logo" width="140px" /></Link>}
                 <ul className="nav">
-                  {showButton &&<li className="nav-item"><Link className="nav-link" to={'/sign-in'}>Login</Link></li>}
-                  {!showButton &&<li className="nav-item"><Link className="nav-link" to={'/display-chart'}>Child Data</Link></li>}
-                  {!showButton && ( <li className="nav-item"> <Link className="nav-link" onClick={() => { handleSignOut();}} to={'/sign-in'}>Logout</Link></li>)}
+                  {showButton &&<li className="nav-item"><Link className="nav-link" to={'/'}>Login</Link></li>}
+                  {!showButton && isAdmin && <li className="nav-item"><Link className="nav-link" to={'/users'}>Users</Link></li>}
+                  {!showButton && isAdmin && <li className="nav-item"><Link className="nav-link" to={'/create'}>New Study</Link></li>}
+                  {!showButton && !isAdmin && <li className="nav-item"><Link className="nav-link" to={'/studyData'}>Study Data</Link></li>}
+                  {!showButton && ( <li className="nav-item"> <Link className="nav-link" onClick={() => { handleSignOut();}} to={'/'}>Logout</Link></li>)}
                 </ul>
 
             </div>
@@ -90,19 +127,26 @@ Hub.listen('auth', listener);
           <div className="auth-wrapper">
             <div className="auth-inner">
               <Routes>
-                <Route element={<PrivateRoutes/>}>
-                  <Route path="/display-chart" component={DisplayChart}/>
-                </Route>
-                <Route path="/"  element={<Home/>} exact/>
-                <Route path="/sign-in" element={<Login toggleButton={toggleButton} handleJwtToken={handleJwtToken} />} />
-                <Route path="/home"  element={<Home/>} exact/>
+                {!showButton && !isAdmin && (
+                  <Route path="/studyData" element={<DisplayChart />} />
+                )}
+                <Route path="/"  element={<Login toggleButton={toggleButton} handleJwtToken={handleJwtToken} />}/>
+                {!showButton && isAdmin && (
+                  <Route path="/users" element={<Users toggleButton={toggleButton} handleJwtToken={handleJwtToken} />} />
+                )}
+                {!showButton && isAdmin && (
+                  <Route path="/create" element={<Create/>} />
+                )}
+                {!showButton && (
+                  <Route path="/home" element={<Home isAdmin={isAdmin} showButton={showButton} />} />
+                )}
+                <Route path="/accessDenied" element={<AccessDenied/>} />
               </Routes>
             </div>
           </div>
 
         </div>
       </Router>
-      
     </div>  
   );
 }
--- website/src/data/login.js
+++ website/src/data/login.js
@@ -1,13 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
-import logo from './neox.svg';
-import axios from 'axios';
+import LoginLogo from './loginLogo.svg';
 //Auth related imports
 import { awsExports } from '../aws-exports';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Auth, Amplify, Logger, Hub  } from 'aws-amplify';
+
+
 
 Amplify.configure({
   Auth: {
@@ -18,105 +19,29 @@ Amplify.configure({
 });
 
 const Login = ({ toggleButton, handleJwtToken }) => {
-    const [jwtToken, setJwtToken] = useState('');
-    const logger = new Logger('Logger', 'INFO');
-
-
-    useEffect(() => {
-        fetchJwtToken();
-    }, []);
-    
-    const fetchJwtToken = async () => {
-        try {
-          const currentUser = await Auth.currentAuthenticatedUser();
-          const session = await Auth.currentSession();
-          const token = session.getIdToken().getJwtToken();
-          setJwtToken(token);
-          handleJwtToken(token); // Pass jwtToken back to App.js
-
-        } catch (error) {
-          console.log('Error fetching JWT token:', error);
-        }
-      };
-    
-
-    const handleStateChange = (state) => {
-      if (state === 'signUp') {
-      // Trigger toggleButton when changing from signIn to signUp
-      toggleButton(false);
-        console.log('Transitioned from signIn to signUp');
-      }
-    };
     
     return (
-        <Authenticator initialState='signIn' onStateChange={handleStateChange}
-        components={{
-            SignUp: {
-            FormFields() {
-
-                return (
-                <>
-                    <Authenticator.SignUp.FormFields />
-
-                    <div><label>Email</label></div>
-                    <input
-                    type="text"
-                    name="email"
-                    //placeholder="Please enter your Email"
-                    />
-                    <div><label>First name</label></div>
-                    <input
-                    type="text"
-                    name="given_name"
-                    //placeholder="Please enter your first name"
-                    />
-                    <div><label>Last name</label></div>
-                    <input
-                    type="text"
-                    name="family_name"
-                    //placeholder="Please enter your last name"
-                    />
-                    <div><label>Middle name</label></div>
-                    <input
-                    type="text"
-                    name="middle_name"
-                    //placeholder="Please enter your middle name"
-                    />
-                    <div><label>Nickname</label></div>
-                    <input
-                    type="text"
-                    name="nickname"
-                    //placeholder="Please enter your Nickname"
-                    />
-                </>
-                );
-            },
-            },
-        }}
-        services={{
-            async validateCustomSignUp(formData) {
-            if (!formData.given_name) {
-                return {
-                given_name: 'First Name is required',
-                };
-            }
-            if (!formData.family_name) {
-                return {
-                family_name: 'Last Name is required',
-                };
-            }
-            if (!formData.email) {
-                return {
-                email: 'Email is required',
-                };
-            }
-            },
-        }}
-        >
-        {({ signOut, user}) => (
-            <Navigate to="/display-chart" />
-        )}
+      <div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ textAlign: 'center' }}>
+              <img src={LoginLogo} alt="NEOX Logo" width="250px" />
+              <h1 style={{ margin: '0', padding: '0' }}>Welcome to Neox Labs</h1>
+              <br></br>
+              <h2 style={{ margin: '0', padding: '0' }}>Log in to continue</h2>
+              <br></br>
+          </div>
+        </div>
+        <Authenticator initialState='signIn' loginMechanisms={['email']} hideSignUp={true}
+          components={{
+          }}
+          services={{
+          }}
+          >
+          {({ signOut, user}) => (
+              <Navigate to="/Home" />
+          )}
         </Authenticator>
+      </div>
   );
 };
 
