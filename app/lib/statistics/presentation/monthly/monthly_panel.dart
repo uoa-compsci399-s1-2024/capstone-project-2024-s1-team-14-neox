26616cb1ae9f436463be358181a9e800b5159c1e
hsir571 <79824690+hsir571@users.noreply.github.com>
2024-05-20 11:51:12 +1200

    Changes

--- app/lib/statistics/presentation/monthly/monthly_panel.dart
+++ app/lib/statistics/presentation/monthly/monthly_panel.dart
@@ -1,3 +1,4 @@
+import 'package:capstone_project_2024_s1_team_14_neox/statistics/domain/statistics_repository.dart';
 import 'package:flutter/material.dart';
 
 class MonthlyPanel extends StatefulWidget {
@@ -7,9 +8,21 @@ class MonthlyPanel extends StatefulWidget {
   State<MonthlyPanel> createState() => _MonthlyPanelState();
 }
 
+
+
 class _MonthlyPanelState extends State<MonthlyPanel> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Container(
+
+
+
+
+
+
+
+
+    );
   }
 }
