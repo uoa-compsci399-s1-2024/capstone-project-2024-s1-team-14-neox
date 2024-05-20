e96e196eea04ce1ebaa7dc9324046edd0899618c
luciaskim <97372163+luciaskim@users.noreply.github.com>
2024-05-20 17:53:48 +1200

    Fix: Remove encryption

--- app/lib/child_home/cubit/child_device_cubit.dart
+++ app/lib/child_home/cubit/child_device_cubit.dart
@@ -91,6 +91,10 @@ class ChildDeviceCubit extends Cubit<ChildDeviceState> {
     required String authorisationCode
   }) async {
     BluetoothDevice? device;
+      late StreamSubscription<BluetoothConnectionState> connectSubscription;
+late StreamSubscription<List<int>> respPeriSubscription;
+late StreamSubscription<BluetoothBondState> bsSubscription;
+    
     try {
       if (FlutterBluePlus.adapterStateNow != BluetoothAdapterState.on) {
         if (Platform.isAndroid) {
@@ -115,7 +119,11 @@ class ChildDeviceCubit extends Cubit<ChildDeviceState> {
       emit(ChildDeviceErrorState(state, "Failed to connect to device: $e"));
       return;
     }
-    
+            bsSubscription = device.bondState.listen((value) {
+        print("FIX bond subscription $value prev:${device?.prevBondState}");
+      });
+       device.cancelWhenDisconnected(bsSubscription);
+
     const String uuidSerivce = "ba5c0000-243e-4f78-ac25-69688a1669b4";
     const List<String> uuidSamples = [
       "42b25f8f-0000-43de-92b8-47891c706106",
