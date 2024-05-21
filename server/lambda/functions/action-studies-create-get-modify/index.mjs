c0b4d6ad991894ae5a41828d13a7a7d7c174a7f6
Hoboneer <gabriel.lisaca@gmail.com>
2024-05-21 22:20:05 +1200

    init studies 4

--- server/lambda/functions/action-studies-create-get-modify/index.mjs
+++ server/lambda/functions/action-studies-create-get-modify/index.mjs
@@ -1,17 +1,15 @@
 import {
   addCorsHeaders,
   connectToDB,
-  generateID,
   authenticateUser,
   AUTH_NONE,
   AUTH_ADMIN,
-  getDBUserIdFromEvent,
+  AUTH_ALL,
 } from "/opt/nodejs/lib.mjs";
 import assert from "node:assert/strict";
 import {
   format,
   isMatch,
-  RangeError,
 } from "date-fns";
 import pg from "pg";
 import {
@@ -26,36 +24,10 @@ let db = await connectToDB();
 const STUDY_METADATA_FIELDS = [
   {"name": "name"        , "required": false},
   {"name": "description" , "required": false},
-  {"name": "start_date"    , "required": true},
+  {"name": "start_date"  , "required": true},
   {"name": "end_date"    , "required": true},
 ];
 const STUDY_DATE_FORMAT = "yyyy-MM-dd";
-
-// async function setMetadataFields(db, resource, studyID, fields)
-// {
-//   let errors = [];
-//   for (let f in fields) {
-
-//     try {
-//       // Inefficient since we only set one field at a time but it's easier to isolate which field is bad.
-//       // We can trust the value of `f` to be a valid and whitelisted column identifier.
-//       await db.query(`UPDATE studies SET ${f} = $2 WHERE id = $1`,
-//                      [studyID, fields[f]]);
-//       console.log(`set field ${f} to "${fields[f]}"`);
-//     } catch (e) {
-//       console.error(e);
-//       errors.push({
-//         resource: `${resource}?fieldvalue=${encodeURIComponent(f)}`,
-//         status: 400,
-//         message: "bad field"
-//       });
-//     }
-//   }
-//   for (let i=0; i<errors.length; i++) {
-//     console.error(`field setting error ${i+1}/${errors.length}: ${JSON.stringify(errors[i])}`);
-//   }
-//   return errors;
-// }
 
 const ACTION_CREATE = "create";
 const ACTION_FETCH = "fetch";
@@ -69,7 +41,6 @@ function readParams(event)
   };
 }
 
-export const handler_create = make_handler(ACTION_CREATE);
 function make_handler(actionId)
 {
   return (async (event) => {
@@ -104,7 +75,6 @@ function make_handler(actionId)
 
     switch (actionId) {
     case ACTION_FETCH: {
-      // TODO
       let res;
       try {
         res = await db.query("SELECT * FROM studies WHERE id = $1", [p.studyID]);
@@ -147,6 +117,8 @@ function make_handler(actionId)
           data: fields,
         }),
       };
+      addCorsHeaders(goodResp);
+      return goodResp;
     }
     case ACTION_CREATE:
     case ACTION_MODIFY: {
@@ -212,7 +184,7 @@ function make_handler(actionId)
       let dbError = null;
       try {
         switch (actionId) {
-        case ACTION_CREATE:
+        case ACTION_CREATE: {
           const columnsList = STUDY_METADATA_FIELDS.map(mf => pg.escapeIdentifier(mf.name)).join(", ");
           // 1+i since PostgreSQL parameters start from 1
           // n + (1+i) to put fields in PostgreSQL parameters *after* `n`
@@ -221,7 +193,8 @@ function make_handler(actionId)
             INSERT INTO studies (id, ${columnsList})
             VALUES ($1, ${queryFieldNumbers.map(n => '$' + n).join(',')})`);
           break;
-        case ACTION_MODIFY:
+        }
+        case ACTION_MODIFY: {
           const sqlUpdateSetExpressions = [];
           let paramNum = 2;
           // the order in the sql param list (needed since object key iteration order is indeterminate)
@@ -241,6 +214,7 @@ function make_handler(actionId)
             };
           }
           break;
+        }
         }
       } catch (e) {
         if (e.code === UNIQUE_VIOLATION && e.constraint === "studies_pkey") {
@@ -294,133 +268,6 @@ function make_handler(actionId)
   });
 }
 
-export const handler_fetch = async (event) => {
-  const childID = event.pathParameters.childID;
-  const resolvedResource = event.resource.replace("{childID}", encodeURIComponent(childID));
-  const body = {};
-  const response = {};
-  console.log(`childID: ${childID}`);
-  assert(event.httpMethod.toUpperCase() === "GET");
-
-  let res;
-  try {
-    res = await db.query("SELECT * FROM children WHERE id = $1", [childID]);
-  } catch (e) {
-    throw e;
-  }
-
-  const auth = await authenticateUser(event, db, AUTH_PARENT_OFCHILD, {"childID": childID});
-  if (auth === AUTH_NONE) {
-    body.errors = [{
-      resource: resolvedResource,
-      status: 403,
-      message: `child ID doesn't exist or user is not authorised to view their personal info`,
-    }];
-    response.statusCode = body.errors[body.errors.length-1].status;
-    response.body = JSON.stringify(body);
-    addCorsHeaders(response);
-    return response;
-  }
-
-  if (res.rows.length === 0){
-    const err = {
-      resource: resolvedResource,
-      status: 403,
-      message: `child ID doesn't exist or user is not authorised to view their personal info`,
-    };
-    console.error(`${childID}: ${err.message}`);
-    body.errors = [err];
-    response.statusCode = 403;
-    response.body = JSON.stringify(body);
-  } else {
-    const fields = res.rows[0];
-    let deletedFields = [];
-    for (let f in fields) {
-      if (!(PERSONAL_INFO_FIELDS_CHILD.includes(f))) {
-        delete fields[f];
-        deletedFields.push(f);
-      }
-    }
-    console.log(`${childID}: deleted the following fields before sending to caller: ${deletedFields}`);
-    if (fields.birthdate != null) {
-      fields.birthdate = format(fields.birthdate, "yyyy-MM-dd");
-    }
-    body.data = fields;
-    response.statusCode = 200;
-  }
-
-  response.body = JSON.stringify(body);
-  addCorsHeaders(response);
-  return response;
-};
-
-export const handler_modify = async (event) => {
-  const childID = event.pathParameters.childID;
-  const infoResource = event.resource.replace("{childID}", encodeURIComponent(childID));
-  const body = {};
-  const response = {};
-  console.log(`childID: ${childID}`);
-  assert(event.httpMethod.toUpperCase() === "PUT" || event.httpMethod.toUpperCase() === "PATCH");
-
-  let personal_info = JSON.parse(event.body);
-  console.log(`got fields: ${JSON.stringify(personal_info)}`);
-
-  const auth = await authenticateUser(event, db, AUTH_PARENT_OFCHILD, {"childID": childID});
-  if (auth === AUTH_NONE) {
-    body.errors = [{
-      resource: infoResource,
-      status: 403,
-      message: `child ID doesn't exist or user is not authorised to modify their personal info`,
-    }];
-    response.statusCode = body.errors[body.errors.length-1].status;
-    response.body = JSON.stringify(body);
-    addCorsHeaders(response);
-    return response;
-  }
-
-  let errors;
-  try {
-    await db.query("BEGIN");
-    if (event.httpMethod.toUpperCase() === "PUT") {
-      for (let i=0; i<PERSONAL_INFO_FIELDS_CHILD.length; i++) {
-        await db.query(
-          // We can trust PERSONAL_INFO_FIELDS_CHILD to only have valid fields.
-          `UPDATE children SET ${PERSONAL_INFO_FIELDS_CHILD[i]} = NULL WHERE id = $1`,
-          [childID]
-        );
-      }
-    }
-    errors = await setPersonalInfoFields(
-      db,
-      infoResource,
-      childID,
-      personal_info
-    );
-  } catch (e) {
-    await db.query("ROLLBACK");
-    response.statusCode = 500;
-    body.errors = {
-      resource: infoResource,
-      status: 500,
-      message: "internal server error"
-    };
-    console.error(e);
-  }
-
-  if (response.statusCode == 500) {
-    // already rolled back
-  } else if (errors.length > 0) {
-    await db.query("ROLLBACK");
-    response.statusCode = 400;
-    body.errors = errors;
-  } else {
-    await db.query("COMMIT");
-    response.statusCode = 204;
-  }
-
-  assert(response.data === undefined, "info replace/update action returned some data but it should never do it");
-
-  response.body = JSON.stringify(body);
-  addCorsHeaders(response);
-  return response;
-};
+export const handler_create = make_handler(ACTION_CREATE);
+export const handler_fetch = make_handler(ACTION_FETCH);
+export const handler_modify = make_handler(ACTION_MODIFY);
