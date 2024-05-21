0ec5759f41478c865be13f6d23fd697341624592
Hoboneer <gabriel.lisaca@gmail.com>
2024-05-22 00:37:30 +1200

    read study DB tables in FuncMetaReadAllTables

--- server/lambda/functions/meta-read-all-tables/index.mjs
+++ server/lambda/functions/meta-read-all-tables/index.mjs
@@ -8,9 +8,15 @@ export const handler = async (event) => {
   const res_users = await db.query("SELECT * FROM users");
   const res_children = await db.query("SELECT * FROM children");
   const res_samples = await db.query("SELECT * FROM samples");
+  const res_studies = await db.query("SELECT * FROM studies");
+  const res_study_researchers = await db.query("SELECT * FROM study_researchers");
+  const res_study_children = await db.query("SELECT * FROM study_children");
   return {
     users: res_users.rows,
     children: res_children.rows,
     samples: res_samples.rows,
+    studies: res_studies.rows,
+    study_researchers: res_study_researchers.rows,
+    study_children: res_study_children.rows,
   };
 };
