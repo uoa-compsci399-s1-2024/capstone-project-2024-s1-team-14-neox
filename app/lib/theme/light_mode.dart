05a07d730afa683c58945b8efb24bd3e8210c476
luciaskim <97372163+luciaskim@users.noreply.github.com>
2024-05-24 11:38:13 +1200

    App: Revert theme colour name

--- app/lib/theme/light_mode.dart
+++ app/lib/theme/light_mode.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
     background:  Colors.white,
-    primary: Color(0xFF9C27B0),
+    primary: Colors.purple.shade500,
     secondary: Colors.purple.shade200,
     tertiary: Colors.grey.shade200,
     inversePrimary: Colors.purple.shade900,
