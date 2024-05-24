2b5655be44170e7a2beb145cd32ff91f055a5fcc
Hoboneer <gabriel.lisaca@gmail.com>
2024-05-25 00:20:21 +1200

    server: Remove trailing comma in SQL table declaration
    This caused syntax errors.

--- server/lambda/functions/meta-setupdb/index.mjs
+++ server/lambda/functions/meta-setupdb/index.mjs
@@ -31,7 +31,7 @@ CREATE TABLE studies (
        description TEXT,
        CONSTRAINT date_interval CHECK (min_date <= max_date),
        -- Ensure all IDs are stored in uppercase
-       CONSTRAINT id_caps CHECK (id = upper(id)),
+       CONSTRAINT id_caps CHECK (id = upper(id))
 );
 CREATE TABLE study_children (
        study_id TEXT NOT NULL,
