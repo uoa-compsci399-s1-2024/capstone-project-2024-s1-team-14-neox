a54f7af92a357a401548fbe58f697d37c62ea551
scho815 <161417533+scho815@users.noreply.github.com>
2024-05-30 17:03:34 +1200

    Updated aws cognito userpool

--- website/src/aws-exports.js
+++ website/src/aws-exports.js
@@ -1,5 +1,5 @@
 export const awsExports = {
     "REGION" : "ap-southeast-2",
-    "USER_POOL_ID": "ap-southeast-2_YaivzAcOS",
-    "USER_POOL_APP_CLIENT_ID": "6nhblb0fiv6n18qabmc2oeg82s"
+    "USER_POOL_ID": "ap-southeast-2_aAlb4qhNA",
+    "USER_POOL_APP_CLIENT_ID": "6m1cc7nicgb0l4m6oj86prqajj"
 }
