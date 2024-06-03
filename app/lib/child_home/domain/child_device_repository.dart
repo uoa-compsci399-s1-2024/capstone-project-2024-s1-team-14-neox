7998a8f48e02be36f19ca11ad4612212ee5f26ff
luciaskim <97372163+luciaskim@users.noreply.github.com>
2024-06-03 14:05:45 +1200

    Classification: Final report on indoor/outdoor classification (#106)
    * Classification: Initial commit with serialised model and docker
    
    Git
    
    * App: Serial print in csv format (include clear)
    
    * Classification: raw data collection, no UV
    
    * Classification: different ml models without uv
    
    * Classification: model creation with uv
    
    * Classification: parse raw data
    
    * Classification: removed light column from old samples
    
    * Classification: training xgb, rf, nb
    
    * App: classifiers
    
    * App: Finalise feature engineering
    
    * App: final models trained
    
    * App: validation, model export, threshold selection
    
    * App: updated model
    
    * App: change repository
    
    * App: classifying using random forest model
    
    * Bluetooth sync debugging
    
    * Bluetooth sync debugging
    
    Hardware&App: debugging
    
    hardware prints
    
    debugging prints for ble connection loop
    
    App: test with random forest
    
    Merge debugging prints
    
    Fixing prints
    
    stash 0602 main
    
    * Remove debugging prints
    
    * Classification: testing and validation of model
    
    * BLE fix
    
    * App + Hardware: remove prints
    
    * Classification: Light gbm model
    
    * Classifier: windowsill test
    
    * Solve merge conflict for ipynb file
    
    * Fix merge
    
    * Classification: retrain smaller model
    
    * Classification: Update report
    
    * Classification: Update smaller classification model
    
    ---------
    
    Co-authored-by: Kevin Lu <>

--- app/lib/child_home/domain/child_device_repository.dart
+++ app/lib/child_home/domain/child_device_repository.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'dart:typed_data';
 import 'package:capstone_project_2024_s1_team_14_neox/child_home/domain/child_device_model.dart';
 
-import 'package:capstone_project_2024_s1_team_14_neox/child_home/domain/classifiers/random_forest.dart';
+import 'package:capstone_project_2024_s1_team_14_neox/child_home/domain/classifiers/light_gbm_small.dart';
 import 'package:capstone_project_2024_s1_team_14_neox/data/entities/arduino_data_entity.dart';
 import 'package:capstone_project_2024_s1_team_14_neox/main.dart';
 import 'package:capstone_project_2024_s1_team_14_neox/statistics/domain/single_week_hourly_stats_model.dart';
@@ -12,8 +12,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../data/entities/child_entity.dart';
 import 'child_device_model.dart';
-import 'classifiers/random_forest.dart';
-
 class ChildDeviceRepository {
   static const int bytesPerSample = 20;
   final SharedPreferences sharedPreferences;
